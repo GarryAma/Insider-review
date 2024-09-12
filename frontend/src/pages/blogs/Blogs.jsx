c84d4bc38616d662848c433606b5c2d2023d4f34
Garry Amasia <amasiagarry23@gmail.com>
2024-09-12 17:55:21 +1000

    fetch data from redux and show them to blog's card

--- frontend/src/pages/blogs/Blogs.jsx
+++ frontend/src/pages/blogs/Blogs.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import SearchBlog from "./SearchBlog";
+import { useFetchBlogsQuery } from "../../redux/services/blogsApi";
+import { Link } from "react-router-dom";
 
 const Blogs = () => {
   const [search, setSearch] = useState("");
   const [category, setCategory] = useState("");
   const [query, setQuery] = useState({ search: "", category: "" });
-  console.log(query);
+  //   console.log(query);
 
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
   };
 
   const handleSearchSubmit = () => setQuery({ search, category });
+
+  //   GETTING DATA BY USING REDUX
+  // default value for blogs is {} because we are expecting object from the backend with message property and posts property
+  const { data: blogs = {}, error, isLoading } = useFetchBlogsQuery(query);
+  //   console.log(blogs.posts);
 
   return (
     <div className="mt-8 container mx-auto">
@@ -21,6 +28,32 @@ const Blogs = () => {
         search={search}
       />
 
+      {error && (
+        <div>
+          <span>{error.toString()}</span>
+        </div>
+      )}
+      {isLoading && (
+        <div>
+          <span>Loading ... </span>
+        </div>
+      )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8">
+        {blogs.posts?.map((singleBlog, index) => (
+          <div key={index} className="border shadow-md">
+            <Link key={index} to={`blog/${singleBlog._id}`}>
+              <img
+                src={singleBlog.coverImg}
+                alt={singleBlog.coverImg}
+                className="w-full sm:h-48 h-72"
+              />
+              <h2 className="p-4 text-xs sm:text-sm md:text-md lg:text-lg">
+                {singleBlog.title}
+              </h2>
+            </Link>
+          </div>
+        ))}
+      </div>
       <div>Blogs cards</div>
     </div>
   );
--- frontend/src/redux/services/blogsApi.js
+++ frontend/src/redux/services/blogsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const blogApi = createApi({
   reducerPath: "blogApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://localhost:8000/api",
+    baseUrl: "http://localhost:8000/api",
     credentials: "include",
   }),
   endpoints: (builder) => ({
